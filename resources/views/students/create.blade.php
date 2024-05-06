970fecdd1120f4f167e1788ee44e40eba47476ab
Afaq <afaqtahir0687@gmail.com>
2024-05-07 02:48:57 +0500

    fixes

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -69,7 +69,6 @@
             </form>
         </div>
     </div>
-    
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://unpkg.com/gijgo@1.9.14/js/gijgo.min.js" type="text/javascript"></script>
